94772549fc621f157b2c8befcc4243f9123b8235
Gaurav Rana <gauravrana@Gauravs-MacBook-Pro.local>
2023-03-20 13:15:06 +0530

    code optimize

--- app.js
+++ app.js
@@ -1,48 +1,66 @@
-let userName = document.getElementById("name");
-let email = document.getElementById("email");
-let div = document.getElementById("nameshow");
 
-let se = 1;
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const nameShowDiv = document.getElementById("nameshow");
 
-let userDetails = [];
+let serialNumber = 1;
+const userDetails = [];
 
-function submit() {
-  let createCreate = document.createElement("div");
-  let nameCreate = document.createElement("p");
-  let emailCreate = document.createElement("p");
-  let SerialNo = document.createElement("p");
-  createCreate.appendChild(SerialNo);
-  createCreate.appendChild(nameCreate);
-  createCreate.appendChild(emailCreate);
+/**
+ * Handles the form submission.
+ */
+function submitForm() {
+  // Create the elements for displaying the user details
+  const userDiv = document.createElement("div");
+  const nameParagraph = document.createElement("p");
+  const emailParagraph = document.createElement("p");
+  const serialNumberParagraph = document.createElement("p");
 
-  let filter = userDetails.filter((user) => {
-    return (user.email === email.value);
-  });
+  // Append the child elements to the user div
+  userDiv.appendChild(serialNumberParagraph);
+  userDiv.appendChild(nameParagraph);
+  userDiv.appendChild(emailParagraph);
 
-  if (!userName.value) {
-    alert("please enter the Name");
-  } else if (!email.value) {
-    alert("please enter your email Id");
-  } else if (filter.length == 1) {
-    alert("User EMail is Already Have");
-  } else {
-    div.appendChild(createCreate);
-    let nameOfUser = (nameCreate.innerText = userName.value);
-    let emailOfUser = (emailCreate.innerText = email.value);
-    SerialNo.innerText = se;
-
-    createCreate.classList.add("flex");
-    let arr = {
-      user: nameOfUser,
-      email: emailOfUser,
-    };
-
-    userDetails.push(arr);
-    console.log(userDetails);
-
-    se++;
-
-    userName.value = "";
-    email.value = " ";
+  // Check if the form input is valid
+  if (!validateForm(nameInput.value, emailInput.value)) {
+    return;
   }
+
+  // Set the text for the user's name, email, and serial number
+  const userName = nameInput.value;
+  const userEmail = emailInput.value;
+  nameParagraph.innerText = userName;
+  emailParagraph.innerText = userEmail;
+  serialNumberParagraph.innerText = serialNumber;
+
+  // Add the user details to the userDetails array
+  const user = { name: userName, email: userEmail };
+  userDetails.push(user);
+
+  // Append the user div to the nameshow div
+  userDiv.classList.add("flex");
+  nameShowDiv.appendChild(userDiv);
+
+  // Increment the serial number and clear the form input fields
+  serialNumber++;
+  nameInput.value = "";
+  emailInput.value = "";
+}
+
+/**
+ * Validates the form input and displays an alert if there is an error.
+ * Returns true if the input is valid, false otherwise.
+ */
+function validateForm(name, email) {
+  if (!name) {
+    alert("Please enter a name.");
+    return false;
+  } else if (!email) {
+    alert("Please enter an email address.");
+    return false;
+  } else if (userDetails.some(user => user.email === email)) {
+    alert("That email address is already in use.");
+    return false;
+  }
+  return true;
 }
